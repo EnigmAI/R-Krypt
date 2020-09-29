1ce7d29d6bc35ccb639b595cfe9a4d55fd73052a
FrostDayee <dhruvgarg01@gmail.com>
2020-09-29 20:29:22 +0530

    initial commit

---
+++ ciphers/rot13.py
@@ -0,0 +1,41 @@
+def encrypt(text):
+    result = ""
+
+    # traverse text
+    for i in range(len(text)):
+        char = text[i]
+
+        # Encrypt uppercase characters
+        if (char.isupper()):
+            result += chr((ord(char) + 13 - 65) % 26 + 65)
+
+        # Encrypt lowercase characters
+        else:
+            result += chr((ord(char) + 13 - 97) % 26 + 97)
+
+    return result
+
+
+def decrypt(text):
+    result = ""
+
+    # traverse text
+    for i in range(len(text)):
+        char = text[i]
+
+        # Encrypt uppercase characters
+        if (char.isupper()):
+            result += chr((ord(char) - 13 - 65) % 26 + 65)
+
+        # Encrypt lowercase characters
+        else:
+            result += chr((ord(char) - 13 - 97) % 26 + 97)
+
+    return result
+
+
+text = "DHRUV"
+print("Text  : " + text)
+print("Shift : 13")
+print("Encryption: " + encrypt(text))
+print("Decryption: " + decrypt(encrypt(text).lower()))
